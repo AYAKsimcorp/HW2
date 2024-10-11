6db47ef9d02f2a6c955eb2b2ac59a405ad78a65b
Andrii Yakushenko <ayak@simcorp.com>
2024-10-11 17:07:24 +0200

    result commit

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -8,9 +8,30 @@ namespace ConsoleApp2
 {
     internal class Program
     {
+        enum TransactionType { Buy, Sell }
         static void Main(string[] args)
         {
 
+            Console.WriteLine("Input the nominal of the trade:");
+            string userInput = Console.ReadLine();
+            int nominal = Int32.Parse(userInput);
+
+            Console.WriteLine("Input the trade price:");
+            userInput = Console.ReadLine();
+            decimal tradePrice = Decimal.Parse(userInput);
+
+            Console.WriteLine("Input the transaction type (Buy or Sell):");
+            userInput = Console.ReadLine();
+            TransactionType trcType = (TransactionType)Enum.Parse(typeof(TransactionType), userInput, true);
+
+            int sign = (trcType == TransactionType.Buy) ? 1 : -1;
+
+            decimal currentValue = sign * nominal * tradePrice;
+
+            Console.WriteLine($"The current value of the transaction is: {currentValue:C}");
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
         }
     }
 }
