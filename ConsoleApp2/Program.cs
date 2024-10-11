dee5c3040b60b38d6d93177cf60889555cc9970e
Andrii Yakushenko <ayak@simcorp.com>
2024-10-11 17:12:34 +0200

    Added profit/loss

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -9,9 +9,9 @@ namespace ConsoleApp2
     internal class Program
     {
         enum TransactionType { Buy, Sell }
+
         static void Main(string[] args)
         {
-
             Console.WriteLine("Input the nominal of the trade:");
             string userInput = Console.ReadLine();
             int nominal = Int32.Parse(userInput);
@@ -24,14 +24,27 @@ namespace ConsoleApp2
             userInput = Console.ReadLine();
             TransactionType trcType = (TransactionType)Enum.Parse(typeof(TransactionType), userInput, true);
 
+            Console.WriteLine("Input the original price of the investment:");
+            userInput = Console.ReadLine();
+            decimal originalPrice = Decimal.Parse(userInput);
+
             int sign = (trcType == TransactionType.Buy) ? 1 : -1;
 
             decimal currentValue = sign * nominal * tradePrice;
 
+            int factor = (trcType == TransactionType.Sell) ? 1 : 0;
+
+            decimal profitLoss = factor * (tradePrice - originalPrice) * nominal;
+
             Console.WriteLine($"The current value of the transaction is: {currentValue:C}");
 
-            Console.WriteLine("Press any key to exit");
+            if (factor == 1)
+            {
+                Console.WriteLine($"The profit or loss from the sale is: {profitLoss:C}");
+            }
+
+            Console.WriteLine("Press any key to exit...");
             Console.ReadKey();
         }
     }
-}
+    }
